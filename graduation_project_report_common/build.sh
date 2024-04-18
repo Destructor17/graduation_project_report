c81230ffdfa8431adbccee9e4b7fc79e6fd8fd0a
Artem Borovik <polzovatellllk@gmail.com>
2024-04-18 12:02:48 +0300

    changes

--- graduation_project_report_common/build.sh
+++ graduation_project_report_common/build.sh
@@ -2,5 +2,4 @@ cd $(dirname $0)/docker
 
 . ../_common.sh
 
-docker-compose up --build || exit 1
 docker-compose run --rm builder sh /build/graduation_project_report_common/docker/_build.sh
--- graduation_project_report_common/watch.sh
+++ graduation_project_report_common/watch.sh
@@ -2,9 +2,6 @@ cd $(dirname $0)/docker
 
 . ../_common.sh
 
-docker-compose up --build || exit 1
-
-
 while true
 do
     docker-compose run --rm builder sh /build/graduation_project_report_common/docker/_build.sh
